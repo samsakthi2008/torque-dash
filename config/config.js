cb2f632deb229f2d711ccfeaaa1072ba3471545a
sakthivel.srinivasan@equiniti.com <sakthivel.srinivasan@equiniti.com>
2024-09-05 19:28:21 +0100

    test 1

--- config/config.js
+++ config/config.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+// Define the path to the certificate file
+const certPath = path.join(__dirname, 'config', 'certs', 'your-cert.pem');
+
+// Read the certificate file
+const cert = fs.readFileSync(certPath).toString();
 let config = {
     port: process.env.PORT || 3000,
     db: {
         uri: process.env.DATABASE_URL || 'postgres://avnadmin:AVNS_a2Y3nNNdnwdtWDvLRnY@pg-6124afa-torque-dash.e.aivencloud.com:19018/defaultdb?sslmode=require',
         options: {
             logging: true
+        },
+        ssl: {
+            rejectUnauthorized: true,  // Set to false if you do not want to verify the server certificate
+            cert: cert
         }
     },
     session: {
