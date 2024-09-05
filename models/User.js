9691cb2f050af16b19630186e87efa9cb28bdb25
sakthivel.srinivasan@equiniti.com <sakthivel.srinivasan@equiniti.com>
2024-09-05 06:03:14 +0100

    test 1

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 
-
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         email: {
@@ -24,9 +23,9 @@ module.exports = (sequelize, DataTypes) => {
         hooks: {
             beforeCreate: hashPassword,
         }
-    }
-    );
-    // create association
+    });
+
+    // Create association
     User.associate = function (models) {
         User.hasMany(models.Session, {
             as: 'Sessions',
@@ -37,26 +36,26 @@ module.exports = (sequelize, DataTypes) => {
 
     // Static method for user data validation
     User.validate = function(user) {
-        const schema = {
-            email: Joi.string().required().email({ minDomainAtoms: 2 }).error(new Error('Please provide a valid email.')),
+        const schema = Joi.object({
+            email: Joi.string().required().email().error(new Error('Please provide a valid email.')),
             password: Joi.string().min(8).required(),
             confirmPassword: Joi.string().valid(Joi.ref('password')).error(new Error('Passwords do not match.'))
-        }
-        return Joi.validate(user, schema);
-    }
+        });
+        return schema.validate(user);
+    };
 
     // Instance method for password comparison
     User.prototype.comparePassword = async function(password) {
-        return await bcrypt.compare(password, this.password)
-    }
+        return await bcrypt.compare(password, this.password);
+    };
 
     return User;
-}
-    
-async function hashPassword (user) {
-     // Hash password before saving to db
-     const SALT_FACTOR = 8;
-     const salt = await bcrypt.genSalt(SALT_FACTOR);
-     let hash = await bcrypt.hash(user.password, salt);
-     await user.setDataValue('password', hash);
+};
+
+async function hashPassword(user) {
+    // Hash password before saving to db
+    const SALT_FACTOR = 8;
+    const salt = await bcrypt.genSalt(SALT_FACTOR);
+    let hash = await bcrypt.hash(user.password, salt);
+    await user.setDataValue('password', hash);
 }
